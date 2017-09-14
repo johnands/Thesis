4888b7a5a23ad5fcb680dcffdce212fce52307ac
johnands <jastende@gmail.com>
2017-09-14 20:44:06 +0200

    .

--- Scripts/Behler/symmetryFunctionsPlot.py
+++ Scripts/Behler/symmetryFunctionsPlot.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from cycler import cycler
+import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+grandParentdir = os.path.dirname(parentdir)
+grandGrandParentdir = os.path.dirname(grandParentdir)
+sys.path.insert(0, parentdir) 
+sys.path.insert(1, grandParentdir)
+sys.path.insert(2, grandGrandParentdir)
+import TensorFlow.Tools.matplotlibParameters
 
 def cutoffFunction(rVector, cutoff, cut=False):   
     
@@ -46,40 +55,50 @@ def G5(Rij, Rik, cosTheta, width, cutoff, thetaRange, inversion):
            cutoffFunction(Rij, cutoff) * cutoffFunction(Rik, cutoff)
 
 # set parameters
-plt.rc('lines', linewidth=1.5)
-plt.rc('axes', prop_cycle=(cycler('color', ['g', 'k', 'y', 'b', 'r', 'c', 'm']) ))
-plt.rc('xtick', labelsize=20)
-plt.rc('ytick', labelsize=20)
-plt.rc('axes', labelsize=25)
+#plt.rc('lines', linewidth=1.5)
+#plt.rc('axes', prop_cycle=(cycler('color', ['g', 'k', 'y', 'b', 'r', 'c', 'm']) ))
+#plt.rc('xtick', labelsize=20)
+#plt.rc('ytick', labelsize=20)
+#plt.rc('axes', labelsize=25)
+
+# change parameters
+"""plt.rc('axes', labelsize=20)
 
 
 ##### fc plot #####
-"""Rij = np.linspace(0, 1, 1000)
+Rij = np.linspace(0, 1, 1000)
 cutoff = 1.0
 plt.plot(Rij, cutoffFunction(Rij, cutoff))
-plt.xlabel(r'$R/R_c$', fontsize=15)
-plt.ylabel(r'$f_c$', fontsize=15)
-#plt.savefig('../Figures/Theory/cutoffFunction.pdf')
-plt.show()"""
+plt.xlabel(r'$r_{ij}/r_c$')
+plt.ylabel(r'$f_c(r_{ij}/r_c)$')
+plt.tight_layout()
+plt.savefig('../../Figures/Theory/cutoffFunction.pdf')
+#plt.show()"""
+
+
+# change parameters
+"""plt.rc('xtick', labelsize=22)
+plt.rc('ytick', labelsize=22)
+plt.rc('axes', labelsize=27)
 
 
 
 ##### G1 plot #####
-"""Rij = np.linspace(0, 14, 1000)
+Rij = np.linspace(0, 14, 1000)
 
 legends = []
 for cutoff in [3.0, 5.0, 7.0, 9.0, 11.0]:
     functionValue = G1(Rij, cutoff)
     functionValue[np.where(Rij > cutoff)[0]] = 0
     plt.plot(Rij, functionValue)
-    legends.append(r'$R_c = %3.1f \, \mathrm{\AA{}}$' % cutoff)
+    legends.append(r'$r_c = %3.1f$' % cutoff)
     plt.hold('on')
     
-plt.legend(legends, prop={'size':20})
-plt.ylabel(r'$G_1$')
+plt.legend(legends, prop={'size':24})
+plt.ylabel(r'$G_i^1$')
 plt.tight_layout()
 #plt.show()
-plt.savefig('../Figures/Theory/G1.pdf')
+plt.savefig('../../Figures/Theory/G1.pdf')
 
 ##### G2 plot #####
 
@@ -92,14 +111,14 @@ for eta in [5.0, 0.9, 0.3, 0.15, 0.07, 0.03, 0.01]:
     functionValue = G2(Rij, eta, cutoff,  Rs)
     functionValue[np.where(Rij > cutoff)[0]] = 0
     plt.plot(Rij, functionValue)
-    legends.append(r'$\eta = %3.2f \, \mathrm{\AA{}}^{-2}$' % eta)
+    legends.append(r'$\eta = %3.2f$' % eta)
     plt.hold('on')
 
-plt.legend(legends, prop={'size':20})
-plt.ylabel(r'$G_2, \, R_s = 0 \, \mathrm{\AA{}}$')
+plt.legend(legends, prop={'size':24})
+plt.ylabel(r'$G_i^2, \; r_s = 0$')
 plt.tight_layout()
 #plt.show()
-plt.savefig('../Figures/Theory/G2_1.pdf')
+plt.savefig('../../Figures/Theory/G2_1.pdf')
 
 plt.figure()
 
@@ -110,14 +129,14 @@ for Rs in [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]:
     functionValue = G2(Rij, eta, cutoff,  Rs)
     functionValue[np.where(Rij > cutoff)[0]] = 0
     plt.plot(Rij, functionValue)
-    legends.append(r'$R_s = %.1f \, \mathrm{\AA{}}$' % Rs)
+    legends.append(r'$r_s = %.1f$' % Rs)
     plt.hold('on')
 
-plt.legend(legends, prop={'size':20})
-plt.xlabel(r'$R_{ij} [\mathrm{\AA{}}]$')
-plt.ylabel(r'$G_2, \, \eta=3.0 \, \mathrm{\AA{}}^{-2}$')
+plt.legend(legends, prop={'size':24})
+plt.xlabel(r'$r_{ij}$')
+plt.ylabel(r'$G_i^2, \; \eta=3.0$')
 plt.tight_layout()
-plt.savefig('../Figures/Theory/G2_2.pdf')
+plt.savefig('../../Figures/Theory/G2_2.pdf')
 
 plt.figure()
 
@@ -129,16 +148,16 @@ for kappa in [0.5, 1.0, 1.5, 2.0]:
     functionValue = G3(Rij, cutoff, kappa)
     functionValue[np.where(Rij > cutoff)[0]] = 0
     plt.plot(Rij, functionValue)
-    legends.append(r'$\kappa = %.1f \, \mathrm{\AA{}}^{-1}$' % kappa)
+    legends.append(r'$\kappa = %.1f$' % kappa)
     plt.hold('on')
 
 plt.plot(Rij, np.zeros(1000), 'k--')
-plt.legend(legends, prop={'size':20})
-plt.xlabel(r'$R_{ij} [\mathrm{\AA{}}]$')
-plt.ylabel(r'$G_3$')
+plt.legend(legends, prop={'size':22})
+plt.xlabel(r'$r_{ij}$')
+plt.ylabel(r'$G_i^3$')
 plt.tight_layout()
 #plt.show()
-plt.savefig('../Figures/Theory/G3.pdf')
+plt.savefig('../../Figures/Theory/G3.pdf')
 
 #plt.savefig('../Figures/Theory/radialSymmFuncs.pdf', dpi=1000)"""
 
@@ -146,6 +165,10 @@ plt.savefig('../Figures/Theory/G3.pdf')
 ##### G4 plot #####
 
 plt.figure()
+
+plt.rc('xtick', labelsize=22)
+plt.rc('ytick', labelsize=22)
+plt.rc('axes', labelsize=27)
 
 theta = np.linspace(0, 2*np.pi, 1000) 
 
@@ -158,12 +181,12 @@ for zeta in [1.0, 2.0, 4.0, 16.0, 64.0]:
     plt.hold('on')
     
 plt.legend(legends, prop={'size':20}, loc=9)
-plt.xlabel(r'$\theta$')
-plt.ylabel(r'$G^4/G^5$ angular part')
+plt.xlabel(r'$\theta_{jik}$')
+plt.ylabel(r'$G_i^\theta$')
 plt.axis([0, 2*180, 0, 2])
 plt.tight_layout()
-plt.show()
-#plt.savefig('../Figures/Theory/G4G5angular1.pdf')
+#plt.show()
+#plt.savefig('../../Figures/Theory/G4G5angular1.pdf')
     
 plt.figure()    
     
@@ -176,12 +199,12 @@ for zeta in [1.0, 2.0, 4.0, 16.0, 64.0]:
     plt.hold('on')
 
 plt.legend(legends, fontsize=25, prop={'size':18}, loc=1)
-plt.xlabel(r'$\theta$')
-plt.ylabel(r'$G^4/G^5$ angular part')
+plt.xlabel(r'$\theta_{jik}$')
+#plt.ylabel(r'$G_i^4/G_i^5$ angular part')
 plt.axis([0, 2*180, 0, 2])
 plt.tight_layout()
-plt.show()
-#plt.savefig('../Figures/Theory/G4G5angular2.pdf')
+#plt.show()
+#plt.savefig('../../Figures/Theory/G4G5angular2.pdf')
 
 
 ##### G5 plot #####
